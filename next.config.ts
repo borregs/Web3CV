cdf6f0661dd2487ace822988ce921e9acab61379
borregs <noreply_bravo@gmx.com>
2026-05-23 01:07:27 -0700

    basepath rerun success

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/Web3CV/',
+  basePath: '/Web3CV',
   productionBrowserSourceMaps: false,
   images: { 
     unoptimized : true ,
