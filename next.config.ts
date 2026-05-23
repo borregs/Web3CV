7696a8b815354f6b5bbd1f0c90d52619cdf1aca6
borregs <noreply_bravo@gmx.com>
2026-05-23 00:31:00 -0700

    workflow activate

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
+  output: 'export',
   productionBrowserSourceMaps: false,
-  images: {
+  images: { unoptimized : true },{
     remotePatterns: [
       {
         protocol: "https",
